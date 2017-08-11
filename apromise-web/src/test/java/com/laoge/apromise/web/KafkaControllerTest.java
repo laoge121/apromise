f991d4647a1b4fcde8f85265f9c61d7fd515a0cd
yuhou <xupei0808@163.com>
2017-08-11 16:03:56 +0800

    测试-kafka

--- apromise-web/src/test/java/com/laoge/apromise/web/KafkaControllerTest.java
+++ apromise-web/src/test/java/com/laoge/apromise/web/KafkaControllerTest.java
@@ -1,10 +1,16 @@
 package com.laoge.apromise.web;
 
 import com.laoge.apromise.SpringBootStartApplication;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Properties;
 
 /**
  * Created by yuhou on 2017/8/11.
@@ -13,4 +19,26 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @WebAppConfiguration
 @SpringApplicationConfiguration(classes = SpringBootStartApplication.class)
 public class KafkaControllerTest {
+
+    @Test
+    public void kafkaProducer() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        for (int i = 0; i < 10; i++) {
+
+            producer.send(new ProducerRecord<String, String>("my-topic-1", Integer.toString(i), Integer.toString(i)));
+        }
+
+        producer.close();
+    }
 }
